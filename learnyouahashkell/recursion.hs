72ca3a35d128033696ad8500d10b5e9d595785a9
yun <liyun@idongjia.cn>
2017-01-16 13:14:45 +0800

    update test code

--- learnyouahashkell/recursion.hs
+++ learnyouahashkell/recursion.hs
@@ -5,3 +5,42 @@ maximum' (x:xs)
     | x > maxTail = x
     | otherwise = maxTail
     where maxTail = maximum' xs
+
+------
+maximumt' :: (Ord a) => [a]->a
+maximumt' [] = error "maximumt of empty list"
+maximumt' [x] = x
+maximumt' (x:xs) = max x (maximumt' xs)
+
+-----
+replicate' ::  (Num i, Ord i) => i -> a ->[a]
+replicate' n x
+    | n <= 0 = []
+    | otherwise = x:replicate' (n - 1) x
+
+-------
+take' :: (Num i, Ord i) => i -> [a] ->[a]
+take' n _
+    | n <= 0 = []
+take' _[]    = []
+take' n (x:xs) = x: take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a ->[a]
+repeat' x = x:repeat' x
+
+--
+zip' :: [a] -> [b] ->[(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
+--
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' _ [] = False
+elem' a (x:xs)
+    | a == x    = True
+    | otherwise = a `elem'` xs
