856cacb3b4d1190dd1e168da79c9e0d9d4d962b9
yun <liyun@idongjia.cn>
2017-01-18 09:35:20 +0800

    update high

--- learnyouahashkell/hightorderfunc.hs
+++ learnyouahashkell/hightorderfunc.hs
@@ -32,3 +32,33 @@ fileter' f (x: xs)
 largestDivisible :: (Integral a) => a
 largestDivisible = head (filter p [100000, 99999..])
     where p x = x `mod` 3829 == 0
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n 
+    | even n = n:chain (n `div` 2)
+    | odd n  = n:chain ( n * 3 + 1 )
+
+numLongChain :: Int
+numLongChain = length (filter (\xs -> length xs > 15) (map chain[1..100]))
+
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl(\acc x -> acc + x) 0 xs
+
+prod' :: (Num a) => [a] ->a
+prod' xs = foldl(\p c-> p*c) 1 xs
+
+sum2' :: (Num a ) => [a] ->a
+sum2' = foldl (+) 0
+
+elem2' :: (Eq a) => a -> [a] -> Bool
+elem2' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+last' :: [a] -> a
+last' = foldl1 (\_ x->x)
+
+sqrtSums :: Int
+sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
+
+($) :: (a -> b) -> a -> b
+f $ x = f x
