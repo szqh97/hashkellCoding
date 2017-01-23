5b4e9dc3e4fc7d3b61370d87e5482ac47c8ca806
yun <liyun@idongjia.cn>
2017-01-23 13:24:10 +0800

    update firstversion fro RPN

---
+++ learnyouahashkell/RPNsolve.hs
@@ -0,0 +1,11 @@
+solveRPN :: String -> Double
+--solveRPN expression = head (foldl foldingFunction [] (words expression))
+solveRPN = head . foldl foldingFunction [] . words
+    where foldingFunction (x:y:ys) "*" = (y * x):ys
+          foldingFunction (x:y:ys) "+" = (y + x):ys
+          foldingFunction (x:y:ys) "-" = (y - x):ys
+          foldingFunction (x:y:ys) "/" = (y / x):ys
+          foldingFunction (x:y:ys) "^" = (y ** x):ys
+          foldingFunction (x:xs) "^" = log x:xs
+          foldingFunction xs "sum" = [sum xs]
+          foldingFunction xs numberString = read numberString:xs
