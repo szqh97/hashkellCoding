2361809bbb1a93e5e13394333c791bd6746d7e69
yun <liyun@idongjia.cn>
2017-02-03 21:52:09 +0800

    update

--- learnyouahashkell/monoids.hs
+++ learnyouahashkell/monoids.hs
@@ -14,3 +14,30 @@ newtype CoolBool = CoolBool {getCoolBool :: Bool }
 helloMe :: CoolBool -> String
 helloMe (CoolBool _) = "hello"
 
+newtype Product a  = Product {getProduct :: a } deriving (Eq, Ord, Read, Show, Bounded)
+instance Num a => Monoid (Product a) where
+    mempty = Product 1
+    Product x `mappend` Product y = Product (x * y)
+ 
+newtype Any = Any {getAny :: Bool} deriving (Eq, Ord, Read, Show, Bounded)
+
+instance Monoid Any where
+    mempty = Any False
+    Any x `mappend` Any y = Any (x || y)
+    {--
+instance Monoid Ordering where
+    mempty = EQ
+    LT `mappend` _ = LT
+    EQ `mappend` y = y
+    GT `mappend` _ = GT
+    --}
+lengthCompare :: String -> String ->Ordering
+lengthCompare x y = (length x `compare` length y) `mappend` (x `compare` y)
+
+newtype First a = First  {getFirst :: Maybe a} deriving (Eq, Ord, Read, Show)
+
+instance Monoid (First a ) where
+    mempty = First Nothing
+    First (Just x) `mappend` _ = First (Just x)
+    First Nothing `mappend` x = x
+    
