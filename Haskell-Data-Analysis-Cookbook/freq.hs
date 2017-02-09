69eb30ef0e220bd29e3056d3541b5f78ffd1189b
szqh97 <szqh97@163.com>
2017-02-09 15:11:54 +0800

    update

---
+++ Haskell-Data-Analysis-Cookbook/freq.hs
@@ -0,0 +1,10 @@
+import Data.List (group, sort)
+
+data Color = Red | Green | Blue deriving (Show, Ord, Eq)
+
+main :: IO ()
+main = do
+    let items = [Red, Green, Green, Blue, Red, Green, Blue]
+    let freq = map (\x -> (head x, length x)) . group . sort $ items
+    print freq
+
