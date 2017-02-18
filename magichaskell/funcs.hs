6fe0f2ea144031c781577faa8c627495ae901fc5
szqh97 <szqh97@163.com>
2017-02-18 16:52:15 +0800

    update

--- magichaskell/funcs.hs
+++ magichaskell/funcs.hs
@@ -5,8 +5,29 @@ minSubList xs m = initSum + minDiff
     initSum = sum $ take m xs
     minDiff = minimum $ scanl (+) 0 $ zipWith (-) shifted xs
 
-nub :: Eq a => [a] -> [a]
-nub [] = []
-nub (x:xs) = if x `elem` xs then nub xs else x:nub xs
-                            
-        
+nubl :: Eq a => [a] -> [a]
+nubl [] = []
+nubl (x:xs) = if x `elem` xs then nubl xs else x:nubl xs
+
+addOneMaybe :: Maybe Int -> Maybe Int
+addOneMaybe Nothing = Nothing
+addOneMaybe (Just a) = Just (a + 1)
+
+addOneList :: [Int] -> [Int]
+addOneList = map (+1)
+
+newtype Identity a = Identity { runIdentity :: a }  deriving Eq
+
+instance Functor Identity where
+    --fmap f idx = Identity (f (runIdentity idx))
+    fmap f = Identity . f .runIdentity
+
+newtype Const a b = Const { getConst :: a } deriving Eq
+
+instance Functor (Const a) where
+    fmap _ (Const v) = Const v
+
+data Position  = Position { positionX :: Double, positionY :: Double }
+
+setPositionX :: Double -> Position -> Position
+setPositionX x' p = p {positionX = x'}
