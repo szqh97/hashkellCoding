d6e939283043f51b2c04a99dad0394457e5b174e
szqh97 <szqh97@163.com>
2017-02-27 08:57:15 +0800

    update

---
+++ magichaskell/queens.hs
@@ -0,0 +1,15 @@
+safe :: Int -> [Int] -> Int -> Bool
+safe _ [] _ = True
+safe x (x1:xs) n = 
+    x /= x1
+    && x /= x1 + n && x /= x1 - n
+    && safe x xs (n+1)
+
+queens :: Int -> [[Int]]
+queens 0 = []
+queens n = [x:y | y <- queens (n-1), x <- [1..8], safe x y 1]
+
+queesN :: Int -> [[Int]]
+queesN n = queens n
+    where queens 0 = []
+          queens m = [x:y | y <- queens (m-1), x <- [1..8], safe x y 1]
